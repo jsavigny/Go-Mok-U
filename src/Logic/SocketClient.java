32e629012ee5c059f1a339e5811e0bc36ec4fb06
Steven Andianto <stevenandianto@gmail.com>
2015-12-02 23:21:15 +0700

    Belum bisa wait tunggu kabar dari server.. langsung close sendiri

--- src/Logic/SocketClient.java
+++ src/Logic/SocketClient.java
@@ -1,10 +1,10 @@
-package Logic;
 /**
  * Created by user on 01/12/2015.
  */
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 public class SocketClient
 {
@@ -16,13 +16,14 @@ public class SocketClient
         {
             System.out.println("Connecting to " + serverName + " on port " + port);
             Socket client = new Socket(serverName, port);
+            client.setSoTimeout(10000000);
             System.out.println("Just connected to " + client.getRemoteSocketAddress());
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
-            //ObjectOutputStream outs = new ObjectOutputStream(outToServer);
-            //ObjectInputStream ins = new ObjectInputStream(inFromServer);
+            ObjectOutputStream outs = new ObjectOutputStream(outToServer);
+            ObjectInputStream ins = new ObjectInputStream(inFromServer);
             System.out.println("Silakan Login terlebih dahulu");
             Scanner scanner = new Scanner(System.in);
             String name = scanner.nextLine();
@@ -33,17 +34,43 @@ public class SocketClient
             //Harus pindah baris setelah scan int
             scanner.nextLine();
             out.writeInt(pilihan);
+            //Pilihan 1 Create room
             if (pilihan == 1){
                 String roomName = scanner.nextLine();
                 out.writeUTF(name);
                 out.writeUTF(roomName);
+                int countPlayer = in.readInt();
+                System.out.println("Jumlah orang room " + roomName + " = " + countPlayer);
+                System.out.println("Wanna Play Right Now? (Y/N)");
+                String play = scanner.nextLine();
+                out.writeUTF(play);
+                String statusplay = in.readUTF();
+                System.out.println(statusplay);
+                if (statusplay.equals("No")) {
+                        System.out.println("Harap Tunggu Jumlah Pemain < 3");
+                }
+                else System.out.println("Selamat Bermain");
+                //Game Start Here
             }
+            //Pilihan 2 Join room
             else if (pilihan == 2){
                 String roomName = scanner.nextLine();
                 out.writeUTF(name);
                 out.writeUTF(roomName);
+                int countPlayer = in.readInt();
+                System.out.println("Jumlah orang room " + roomName + " = " + countPlayer);
+                System.out.println("Wanna Play Right Now? (Y/N)");
+                String play = scanner.nextLine();
+                out.writeUTF(play);
+                String statusplay = in.readUTF();
+                System.out.println(statusplay);
+                if (statusplay.equals("No")) {
+                    System.out.println("Harap Tunggu Jumlah Pemain < 3");
+                }
+                else System.out.println("Selamat Bermain");
             }
-            client.close();
+
+            //client.close();
         }catch(IOException e)
         {
             e.printStackTrace();
