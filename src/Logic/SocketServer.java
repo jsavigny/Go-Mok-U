7c6e2ca1e10f8600b0a567999e46a9d7c05a9277
Steven Andianto <stevenandianto@gmail.com>
2015-12-03 21:21:04 +0700

    Dah gabungan datawriter sama printwriter

--- src/Logic/SocketServer.java
+++ src/Logic/SocketServer.java
@@ -10,8 +10,12 @@ public class SocketServer extends Thread
     // The client socket.
     private static Socket clientSocket = null;
     // This chat server can accept up to maxClientsCount clients' connections.
-    private static final int maxClientsCount = 100;
+    private static final int maxClientsCount = 5;
     private static final clientThread[] threads = new clientThread[maxClientsCount];
+    private static int countClient = 0;
+    public static ArrayList<String> user = new ArrayList<>();
+    public static ArrayList<ArrayList<String>> room = new ArrayList<ArrayList<String>>();
+    public static ArrayList<String> selector = new ArrayList<String>();
     public SocketServer(int port) throws IOException
     {
         serverSocket = new ServerSocket(port);
@@ -29,11 +33,18 @@ public class SocketServer extends Thread
             try {
                 clientSocket = serverSocket.accept();
                 int i = 0;
+
                 for (i = 0; i < maxClientsCount; i++) {
                     if (threads[i] == null) {
                         (threads[i] = new Logic.clientThread(clientSocket, threads)).start();
+                        countClient++;
                         break;
                     }
+                }
+                //System.out.println("Jumlah client Thread Connected :"+countClient);
+                for (int j=0; j<countClient; j++){
+                    System.out.println("Jumlah client Thread Connected :"+countClient);
+                    System.out.println("Client Connected :"+threads[j]);
                 }
             } catch (IOException e) {
                 System.out.println(e);
--- src/Logic/clientThread.java
+++ src/Logic/clientThread.java
@@ -12,38 +12,36 @@ public class clientThread extends Thread {
     private Socket clientSocket = null;
     private final clientThread[] threads;
     private int maxClientsCount;
-    public static ArrayList<String> user = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> room = new ArrayList<ArrayList<String>>();
-    public static ArrayList<String> selector = new ArrayList<String>();
+
     public clientThread(Socket clientSocket, clientThread[] threads) {
         this.clientSocket = clientSocket;
         this.threads = threads;
         maxClientsCount = threads.length;
     }
     public void login(String name){
-        user.add(name);
+        SocketServer.user.add(name);
     }
     public void createRoom(String playerName, String roomName){
         int idx = -1;
         boolean found = false;
-        for (int i=0;i<room.size();i++){
-            found = room.get(i).contains(roomName);
+        for (int i=0;i<SocketServer.room.size();i++){
+            found = SocketServer.room.get(i).contains(roomName);
             if( found == true){
                 idx = i;
             }
         }
         if(idx == -1) {
-            selector = new ArrayList<String>();
-            selector.add(roomName);
-            selector.add(playerName);
-            room.add(selector);
+            SocketServer.selector = new ArrayList<String>();
+            SocketServer.selector.add(roomName);
+            SocketServer.selector.add(playerName);
+            SocketServer.room.add(SocketServer.selector);
         }
     }
     public void joinRoom(String playerName, String roomName){
         int idx = -1;
         boolean found = false;
-        for (int i=0;i<room.size();i++){
-            found = room.get(i).contains(roomName);
+        for (int i=0;i<SocketServer.room.size();i++){
+            found = SocketServer.room.get(i).contains(roomName);
             if( found == true){
                 System.out.println("Masuk contains");
                 idx = i;
@@ -52,26 +50,26 @@ public class clientThread extends Thread {
         System.out.println(idx);
         if(idx != -1) {
             System.out.println("MASUK ROOM");
-            room.get(idx).add(playerName);
+            SocketServer.room.get(idx).add(playerName);
         }
     }
     public int countPlayer(String roomName){
         int idx = -1;
         boolean found = false;
-        for (int i=0;i<room.size();i++){
-            found = room.get(i).contains(roomName);
+        for (int i=0;i<SocketServer.room.size();i++){
+            found = SocketServer.room.get(i).contains(roomName);
             if( found == true){
                 idx = i;
             }
         }
-        int count = room.get(idx).size();
+        int count = SocketServer.room.get(idx).size();
         return count-1;
     }
     public void Play(String roomName){
         int idx = -1;
         boolean found = false;
-        for (int i=0;i<room.size();i++){
-            found = room.get(i).contains(roomName);
+        for (int i=0;i<SocketServer.room.size();i++){
+            found = SocketServer.room.get(i).contains(roomName);
             if( found == true){
                 idx = i;
             }
@@ -80,23 +78,23 @@ public class clientThread extends Thread {
         for (int j=1;j<countPlayer(roomName)+1;j++){
             if(j == 1) {
                 race = "O";
-                System.out.println(room.get(idx).get(j) + " NOW PLAYING "+race);
+                System.out.println(SocketServer.room.get(idx).get(j) + " NOW PLAYING "+race);
             }
             else if(j == 2) {
                 race = "X";
-                System.out.println(room.get(idx).get(j) + " NOW PLAYING "+race);
+                System.out.println(SocketServer.room.get(idx).get(j) + " NOW PLAYING "+race);
             }
             else if(j == 3) {
                 race = "^";
-                System.out.println(room.get(idx).get(j) + " NOW PLAYING "+race);
+                System.out.println(SocketServer.room.get(idx).get(j) + " NOW PLAYING "+race);
             }
             else if(j == 4) {
                 race = "$";
-                System.out.println(room.get(idx).get(j) + " NOW PLAYING "+race);
+                System.out.println(SocketServer.room.get(idx).get(j) + " NOW PLAYING "+race);
             }
             else if(j == 5) {
                 race = "#";
-                System.out.println(room.get(idx).get(j) + " NOW PLAYING "+race);
+                System.out.println(SocketServer.room.get(idx).get(j) + " NOW PLAYING "+race);
             }
         }
     }
@@ -109,20 +107,19 @@ public class clientThread extends Thread {
                 DataInputStream in = new DataInputStream(clientSocket.getInputStream());
                 DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
                 //Terima variable dari client
-                //ObjectInputStream ins = new ObjectInputStream(clientSocket.getInputStream());
+                ObjectInputStream ins = new ObjectInputStream(clientSocket.getInputStream());
                 //Kirim variable ke client
-                //ObjectOutputStream outs = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectOutputStream outs = new ObjectOutputStream(clientSocket.getOutputStream());
                 String nama = in.readUTF();
                 System.out.println("Berhasil Login " + nama);
                 login(nama);
-                System.out.println(user);
+                System.out.println(SocketServer.user);
                 out.writeUTF("Welcome " + nama + " Lets Gomoku!");
-                out.writeInt(room.size());
-                for(int i=0;i<room.size();i++){
-                    out.writeUTF(room.get(i).get(0));
-                }
-
                 int pilihan = in.readInt();
+                out.writeInt(SocketServer.room.size());
+                for(int i=0;i<SocketServer.room.size();i++){
+                    out.writeUTF(SocketServer.room.get(i).get(0));
+                }
                 System.out.println("Pilihan Client : " + pilihan);
                 if (pilihan == 1){
                     String namacreator = in.readUTF();
@@ -130,7 +127,7 @@ public class clientThread extends Thread {
                     System.out.println("Nama Creator :"+namacreator);
                     System.out.println("Nama Room :" + namaroom);
                     createRoom(namacreator, namaroom);
-                    System.out.println(room);
+                    System.out.println(SocketServer.room);
                     out.writeInt(countPlayer(namaroom));
                     String play = in.readUTF();
                     if((play.equals("y") || play.equals("Y")) && countPlayer(namaroom) >= 3){
@@ -148,7 +145,7 @@ public class clientThread extends Thread {
                     System.out.println("Nama Joiner :"+namajoin);
                     System.out.println("Nama Room :" + namaroom);
                     joinRoom(namajoin, namaroom);
-                    System.out.println(room);
+                    System.out.println(SocketServer.room);
                     out.writeInt(countPlayer(namaroom));
                     String play = in.readUTF();
                     if((play.equals("y") || play.equals("Y")) && countPlayer(namaroom) >= 3){
